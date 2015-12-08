8bf7ab52f57474733565dd16d5351afa5ddcc4b7
gongliang <gongliang@aprbrother.com>
2015-12-08 14:42:00 +0800

    first commit

---
+++ WallPaper/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  WallPaper
+//
+//  Created by gongliang on 15/12/4.
+//  Copyright © 2015年 AB. All rights reserved.
+//
+
+import UIKit
+import CoreLocation
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
